7473c618f8da4d86f459c916e28df8a80a972280
zaferguvenn <zafer.guven58@gmail.com>
2025-02-01 21:17:03 +0300

    Embracing canvas.clipOutPath
    Incorporating canvas.clipOutPath for API level 26 and above ensures hardware acceleration remains enabled while CDP effectively creates a hole in the drawing, allowing for a cleaner and more efficient rendering process.

--- app/src/main/java/com/example/tutorialpagewithspotlighteffect/SpotlightView.kt
+++ app/src/main/java/com/example/tutorialpagewithspotlighteffect/SpotlightView.kt
@@ -4,29 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
+import android.graphics.Region
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.pm.ShortcutInfoCompat.Surface
 
 class SpotlightView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
-    private val backgroundPaint = Paint().apply {
-        color = Color.parseColor("#B3000000") // Yarı şeffaf siyah
-        style = Paint.Style.FILL
-    }
 
-    init {
-        // disable hardware acceleration
-        // This method is
-        // Required for
-        // "PorterDuffXfermode(PorterDuff.Mode.CLEAR)"
-        // to work properly
-        setLayerType(LAYER_TYPE_SOFTWARE, null)
-    }
+    // Translucent black color for coating
+    private val overlayColor =
+        Color.parseColor("#B3000000")
 
-    private val clearPaint = Paint().apply {
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    }
     private var spotlightX = 0f
     private var spotlightY = 0f
     private var spotlightRadius = 0f
@@ -41,9 +33,35 @@ class SpotlightView(context: Context, attrs: AttributeSet? = null) : View(contex
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        // Dim full screen
-        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), backgroundPaint)
-        // Clear Spotlight region (make transparent)
-        canvas.drawCircle(spotlightX, spotlightY, spotlightRadius, clearPaint)
+
+        // Save canvas state
+        canvas.save()
+
+        // Path that will create the
+        // Spotlight hole area
+        val circlePath = Path().apply{
+            addCircle(
+                spotlightX,
+                spotlightY,
+                spotlightRadius,
+                Path.Direction.CCW
+            )
+        }
+
+        // Different method for API 26 and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            canvas.clipOutPath(circlePath)
+        } else {
+            @Suppress("DEPRECATION")
+            canvas.clipPath(circlePath, Region.Op.DIFFERENCE)
+        }
+
+        // Draw an overlay on the clipped area
+        // (all area except the hole)
+        canvas.drawColor(overlayColor)
+
+        // Restore canvas state
+        canvas.restore()
     }
 }
